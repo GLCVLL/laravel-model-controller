faeb26c0ff794e71c5ff65b207957ec0b026db6b
GLCVLL <g.ioele96@outlook.it>
2023-08-22 20:56:28 +0200

    added movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,24 @@
 @extends('layouts.main')
 
 @section('title', 'Home')
+
+
+@section('main')
+    <div class="title text-center">
+        <h1>The Best Movies</h1>
+    </div>
+    <div class="row row-cols-2 my-2">
+        @forelse ($movies as $movie)
+            <div class="col">
+                <div class="card my-2">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                        <p class="card-text">Vote: {{ $movie->vote }}</p>
+                    </div>
+                </div>
+            </div>
+        @empty
+        @endforelse
+    </div>
+@endsection
